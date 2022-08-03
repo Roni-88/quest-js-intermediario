9d4d9b26f79411447d1633794d6be3d0ea7fe37a
Roni 88 <ronix88x@hotmail.com>
2022-08-02 22:26:02 -0300

    modificações gerais

---
+++ formulario.js
@@ -0,0 +1,6 @@
+//Obj 1 - Ao clicar enviar se não tiver preenchido o campo, mostrar a msg de erro e pintar o campo de vermelho.
+
+
+
+
+//Obj 2 - Ao clicar, estando tudo preenchido, dar ok e deixar a borda verde.
