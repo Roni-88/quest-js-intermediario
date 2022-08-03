677d7c7687c598dce9a62f752ee20c5513277dcd
Roni 88 <ronix88x@hotmail.com>
2022-08-02 22:55:32 -0300

    objetivos

--- formulario.js
+++ formulario.js
@@ -1,6 +1,8 @@
-//Obj 1 - Ao clicar enviar se não tiver preenchido o campo, mostrar a msg de erro e pintar o campo de vermelho.
+//Obj 1 - Ao clicar enviar se não tiver preenchido o campo, mostrar a msg de erro (campo obrigatório) embaixo de cada input e pintar o campo de vermelho.
 
+//Quando o usuário clicar, é necessário que o programa verifique se os campos (input e textarea) estão vazios. Em caso positivo, tranformar a borda dos campos em vermelha e fazer aparecer uma msg de erro abaixo deles.
 
+//Obj 2 - Ao clicar, estando tudo preenchido, deixar a borda verde.
 
+//Quando o usuário clicar, e o valor retornado for diferente de vazio, colocar uma borda verde nos campos (input e textarea).
 
-//Obj 2 - Ao clicar, estando tudo preenchido, dar ok e deixar a borda verde.
